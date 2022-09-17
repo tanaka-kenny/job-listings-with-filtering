8ce2e8f50f929351d4b21a3b8ec0696257a20d1f
Perez <tanakakenne2000@gmail.com>
2022-09-17 19:27:18 +0200

    now rendering the job values dynamically

--- src/components/JobListing.js
+++ src/components/JobListing.js
@@ -1,32 +1,47 @@
 import './JobListing.css';
 
-function JobListing() {
+function JobListing({ job }) {
+    job =   {
+        "id": 1,
+        "company": "Photosnap",
+        "logo": "./images/photosnap.svg",
+        "new": true,
+        "featured": true,
+        "position": "Senior Frontend Developer",
+        "role": "Frontend",
+        "level": "Senior",
+        "postedAt": "1d ago",
+        "contract": "Full Time",
+        "location": "USA Only",
+        "languages": ["HTML", "CSS", "JavaScript"],
+        "tools": []
+      }
 
   return (
     <div className='jobListing'>
         <div className="left-col">
             <div className="avatar">
-                <img src="./images/photosnap.svg" alt="" />
+                <img src={job.logo} alt="" />
             </div>
             <div className="info">
                 <p>
-                    <span className="company-name">Photosnap</span>
-                    <span className='new'>NEW!</span>
-                    <span className='featured'>FEATURED</span>
+                    <span className="company-name">{job.company}</span>
+                    {job.new && <span className='new'>NEW!</span> }
+                    {job.featured && <span className='featured'>FEATURED</span>}
                 </p>
                 <h4>Senior Frontend Developer</h4>
                 <p className='bottomLine'>
-                    <span>1 day ago</span> <div className="dot"></div>
-                    <span>USA</span>  <div className="dot"></div>
-                    <span>Remote</span> <div className="do"></div>
+                    <span>{job.postedAt}</span> <div className="dot"></div>
+                    <span>{job.contract}</span> <div className="dot"></div>
+                    <span>{job.location}</span> 
                 </p>
             </div>
         </div>
         <div className="right-col">
             <div className="filters">
-                <div className="f">Frontend</div> 
-                <div className="f">Senior</div> 
-                <div className="f">HTML</div> 
+                {
+                    job.languages.map(l => <div className="f">{l}</div> )
+                }
             </div>
         </div>
 
